ff0950945aa4c0c7d2d773c46c027d2c89c11e5a
Kevin Buchholz <buchholz.kevin@gmail.com>
2023-05-19 15:01:28 -0400

    Initial Commit

---
+++ recreate/recreateApp.swift
@@ -0,0 +1,17 @@
+//
+//  recreateApp.swift
+//  recreate
+//
+//  Created by Kevin Buchholz on 5/19/23.
+//
+
+import SwiftUI
+
+@main
+struct recreateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
